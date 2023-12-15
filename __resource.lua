cd106e791d1e32393a35ff3f5a2679201f3e3bcb
BobbySwagg <49619297+BobbySwaggTV@users.noreply.github.com>
2023-12-15 12:15:15 -0600

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,5 +1,5 @@
 --------------------------------
-------- Created by Hamza -------
+--- Created by BobbySwaggTV ----
 -------------------------------- 
 
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
